87bbdfd47fdb05770bf45630821c6fa02a075acb
Kieran Hunt <kieran.hunt92@gmail.com>
2019-01-30 20:06:43 +0200

    Move JS into its own file

---
+++ app.js
@@ -0,0 +1,28 @@
+(function() {
+  console.log('📓: https://github.com/KieranHunt/kieran.casa')
+
+  // Pick profile picture
+
+  var id = 'default-picture'
+  const date = new Date()
+  if (date.getMonth() === 9)
+      id = 'halloween-picture'
+  if (date.getMonth() === 11)
+      if (date.getDay() <= 24)
+          id = 'christmas-picture'
+      
+  document.getElementById(id).classList.remove('hidden')
+
+  // Set current weather
+
+  fetch('/api/weather.json')
+    .then(response => response.json())
+    .then(function(weather) {
+      tempInFarenheit = weather.currently.apparentTemperature
+      tempInCelcius = Math.trunc((tempInFarenheit - 32.0) * (5/9))
+      
+      tempElement = document.getElementById('tempurature')
+      tempElement.innerHTML = `${tempInCelcius}°C`
+      tempElement.classList.remove('hidden')
+    });
+})();
