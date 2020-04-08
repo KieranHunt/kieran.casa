f2ede44c4c736d6c6b69d7b7df3d854dd8eec2b6
Kieran Hunt <hunkiera@amazon.com>
2020-04-08 15:10:51 +0200

    Add Clicky analytics

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 (function() {
-  console.log('📓: https://github.com/KieranHunt/kieran.casa')
+  console.log("📓: https://github.com/KieranHunt/kieran.casa");
 
   // Pick profile picture
 
-  var id = 'default'
-  const date = new Date()
-  if (date.getMonth() === 9)
-      id = 'halloween'
-  if (date.getMonth() === 11)
-      if (date.getDay() <= 24)
-          id = 'christmas'
-      
-  element = document.getElementById('picture')
-  url = element.getAttribute(`data-${id}`)
-  element.style.backgroundImage = `url(${url})`
+  var id = "default";
+  const date = new Date();
+  if (date.getMonth() === 9) id = "halloween";
+  if (date.getMonth() === 11) if (date.getDay() <= 24) id = "christmas";
+
+  element = document.getElementById("picture");
+  url = element.getAttribute(`data-${id}`);
+  element.style.backgroundImage = `url(${url})`;
 
   // Set current weather
 
-  fetch('/api/weather.json')
-    .then(response => response.json())
+  fetch("/api/weather.json")
+    .then((response) => response.json())
     .then(function(weather) {
-      tempInFarenheit = weather.currently.apparentTemperature
-      tempInCelcius = Math.trunc((tempInFarenheit - 32.0) * (5/9))
-      
-      tempElement = document.getElementById('tempurature')
-      tempElement.innerHTML = `${tempInCelcius}°C`
-      tempElement.classList.remove('hidden')
+      tempInFarenheit = weather.currently.apparentTemperature;
+      tempInCelcius = Math.trunc((tempInFarenheit - 32.0) * (5 / 9));
+
+      tempElement = document.getElementById("tempurature");
+      tempElement.innerHTML = `${tempInCelcius}°C`;
+      tempElement.classList.remove("hidden");
     });
 })();
