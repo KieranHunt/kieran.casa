3f1eaa86b7beef03a968b6056a7ab364b25a351f
Kieran Hunt <hunkiera@amazon.com>
2020-04-16 17:24:32 +0200

    Add and configure PurgeCSS for Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,19 @@
+const path = require("path");
+
 module.exports = {
-	plugins: [`gatsby-plugin-postcss`, `gatsby-plugin-mdx`],
+	plugins: [
+		`gatsby-plugin-postcss`,
+		`gatsby-plugin-mdx`,
+		{
+			resolve: `gatsby-plugin-purgecss`,
+			options: {
+				printRejected: true,
+				tailwind: true,
+				whitelist: [""],
+				content: [
+					path.join(process.cwd(), "src/**/!(*.d).{ts,js,jsx,tsx,css}"),
+				],
+			},
+		},
+	],
 };
